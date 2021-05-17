019c4c2f931e53f403334e30983b8c76cb1a44f5
DB van de Pavert <danielvdpavert@gmail.com>
2021-05-17 14:11:42 +0200

    Dataloader created
    + Initial dataloader

--- code/dataloader.py
+++ code/dataloader.py
@@ -10,26 +10,54 @@ something something main description
 
 import pandas
 import os
+import json
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer, BertForPreTraining
 
 os.chdir("..")
 
 
-def load_data(file):
-    df_test = pandas.read_json(file, orient='records')
-    s = format_data(df_test)
-    return s
+class FriendsDataset(Dataset):
+
+    def __init__(self, json_file, tokenizer=None):
+        """
+        Args:
+            json_file (string): Path to the json file with annotations.
+        """
+
+        with open(json_file) as f:
+            data = json.load(f)
+
+        self.foo = []
+
+        for e in data["episodes"]:
+            for s in e["scenes"]:
+
+                zap = []
+
+                for u in s["utterances"]:
+                    bar = tokenizer(u['transcript'])
+                    if len(u['speakers']) == 0:
+                        speaker = 'none'
+                    else:
+                        speaker = u['speakers'][0]
+                    zap.append([s["scene_id"], speaker, bar])
+                self.foo.append(zap)
+
+    def __len__(self):
+        return len(self.foo)
+
+    def __getitem__(self, idx):
+        return self.foo[idx]
 
 
-def get_stats(df):
-    print(df.describe())
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+dataset = FriendsDataset(json_file='data/json/friends_season_01.json', tokenizer=tokenizer.encode)
 
+dataloader = DataLoader(dataset, shuffle=True, num_workers=0)  # Some weird stuff with the inclusion of batch sizes
 
-def format_data(df):
-    something = {}
-
-    for scene in df.T:
-        for line in df.T[scene]:
-            if line:
-                something[line['speaker']] = line['utterance']
-
-    return something
+for x in dataloader:
+    print(x)
+    break
--- code/main.py
+++ code/main.py
@@ -12,16 +12,20 @@ from dataloader import *
 from preprocess import *
 from models import *
 
-test_file = 'data/friends_test.json'
-train_file = 'data/friends_train.json'
-dev_file = 'data/friends_dev.json'
+# test_file = 'data/friends_test.json'
+# train_file = 'data/friends_train.json'
+# dev_file = 'data/friends_dev.json'
+
+file = 'data/json/friends_season_01.json'
 
 
 def main(input):
-    df = load_data(test_file)
-    df_clean = preprocess(df, ["flags"])
-    print(df_clean)
+    # df = load_data(file)
+
+    # df_clean = preprocess(df, ["flags"])
+    # print(df_clean)
     # show_scene(df, 8)
+    print('WIP')
 
 
 def show_scene(df, scene):
